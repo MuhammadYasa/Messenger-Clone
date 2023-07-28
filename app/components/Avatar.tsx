a43eb8a024b6b60584d10cfe396cefe2b32795d6
muhammad yasa <nearhxh@gmail.com>
2023-07-28 17:14:59 +0700

    update online icon

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -17,7 +17,7 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
           fill
         />
         {/* trial test hardcore online status on profile picture */}
-        <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
+        <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-1 right-1 h-2 w-2 md:h-3 md:w-3" />
       </div>
     </div>
   );
